dd84deaa0d8cbc6e505fc7e0b664eb0cb176dfb7
Avinash Penugonda <avi.reddy001@gmail.com>
2026-04-16 18:30:20 -0500

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        //  [strs], group all anagrams into sublists
+        // output: any order,
+
+        // sort them, n * m
+        // space: O(n)
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String str: strs) {
+            int[] charArray = new int[26];
+            for (char c: str.toCharArray()) {
+                charArray[c - 'a']++;
+            }
+            String key = Arrays.toString(charArray);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
