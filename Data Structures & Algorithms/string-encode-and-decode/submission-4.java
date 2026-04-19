7f0e2bdd715be6c610600a7d9c323eacc54ff2ac
Avinash Penugonda <avi.reddy001@gmail.com>
2026-04-19 12:54:02 -0500

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    /*
+        Encode
+         - ["hi", "this", "remembered"]
+         2#hi4#this10#remembered
+
+         Decode
+            - while(isDigit) find the length
+            - substring of the word until the length
+    */
+
+    public String encode(List<String> strs) {
+        StringBuilder outputString = new StringBuilder();
+
+        for(String str: strs) {
+            int length = str.length();
+            outputString.append(length);
+            outputString.append("#");
+            outputString.append(str);
+        }
+        return outputString.toString();
+    }
+
+    public List<String> decode(String str) {
+        System.out.println(str);
+        List<String> output = new ArrayList<>();
+
+        StringBuilder lengthStr = new StringBuilder();
+        int j = 0;
+        while (j < str.length()) {    
+            
+            // 2#hi4#this10#remembered
+            while(Character.isDigit(str.charAt(j))){
+                lengthStr.append(str.charAt(j));
+                j++;
+            }
+            Integer size = Integer.valueOf(lengthStr.toString()); // 2
+            int start = j + 1; // 2
+            int end = start + size; // 4
+            String word = str.substring(start, end);
+            output.add(word);
+            j = end;
+            lengthStr = new StringBuilder();
+            
+        }
+        return output;
+    }
+}
