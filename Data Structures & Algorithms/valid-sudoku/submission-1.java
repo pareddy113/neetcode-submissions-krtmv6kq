fcd44fddc3dde871e1d30578160a67d7625fa2e8
Avinash Penugonda <avi.reddy001@gmail.com>
2026-04-20 20:56:29 -0500

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,48 @@
+class Solution {
+    /*
+        9x9 board
+        row unique,
+        col unique,
+        sub-box unique
+
+        HashMap<Integer, HashSet<Character>>
+
+        0 - 9
+        0,1,2
+        0,1,2
+
+        0,1,2
+        3,4,5
+    */
+    public boolean isValidSudoku(char[][] board) {
+        
+        Map<Integer, Set<Character>> rowSet = new HashMap<>();
+        Map<Integer, Set<Character>> colSet = new HashMap<>();
+        Map<Integer, Set<Character>> blockSet = new HashMap<>();
+        
+        for(int r = 0; r < board.length; r++) {
+            for(int c = 0; c < board[0].length; c++) {
+                // figure row
+                int blockNum = ((r / 3) * 3) + (c / 3);
+                rowSet.putIfAbsent(r, new HashSet<Character>());
+                colSet.putIfAbsent(c, new HashSet<Character>());
+                blockSet.putIfAbsent(blockNum, new HashSet<Character>());
+
+                if (board[r][c] == '.') {
+                    continue;
+                }
+                if(rowSet.get(r).contains(board[r][c]) || colSet.get(c).contains(board[r][c]) || blockSet.get(blockNum).contains(board[r][c])){
+                    return false;
+                }
+                rowSet.get(r).add(board[r][c]);
+                colSet.get(c).add(board[r][c]);
+                blockSet.get(blockNum).add(board[r][c]);
+
+                System.out.println(rowSet);
+                System.out.println(colSet);
+                System.out.println(blockSet);
+        }
+    }
+    return true;
+  }
+}
