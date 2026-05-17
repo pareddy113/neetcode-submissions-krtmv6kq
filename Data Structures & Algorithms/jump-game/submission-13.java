ca68167f002a4c07cab0439ae1dee6bf60577b2d
Avinash Penugonda <avi.reddy001@gmail.com>
2026-05-17 15:57:38 -0500

    Add: jump-game - submission-13

---
+++ Data Structures & Algorithms/jump-game/submission-13.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        
+        Integer maxJump = 0;
+
+        /*
+            1,3         
+        */
+        for(int i = 0; i < nums.length; i++) {
+            
+            if (i > maxJump) {
+                return false;
+            }
+
+            maxJump = Math.max(maxJump, i + nums[i]);
+
+            if (maxJump >= nums.length - 1) {
+                return true;
+            }
+        }
+        return true;
+    }
+}
